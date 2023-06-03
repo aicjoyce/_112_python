e7f3b529f075743ef9452cc410d0aab9ea571b10
aicjoyce <amytedpc@gmail.com>
2023-06-03 14:17:27 +0800

    modify

--- lesson10_1.py
+++ lesson10_1.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
-if st.button("say hello",key="myButton"):
+
+def button_click():
+    st.write(st.session_state)
+if st.button("say hello",key="myButton",on_click=button_click):
     st.write("why hello there")
 else:
     st.write("Goodbye")
